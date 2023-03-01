453d340a49a9077ba7a65685ef4ff205a43741e4
nathanhoyes <07nhoyes@gmail.com>
2023-03-01 23:00:09 +0000

    First Commit

---
+++ src/Display/Display.py
@@ -0,0 +1,28 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import ST7735
+import time
+
+disp = ST7735.ST7735(port=0, cs=0, dc=24, backlight=13, rst=25, width=128, height=160, rotation=0, invert=False)
+
+WIDTH = disp.width
+HEIGHT = disp.height
+
+disp._offset_left= 0
+disp._offset_top= -1
+
+img = Image.new('RGB', (WIDTH, HEIGHT))
+draw = ImageDraw.Draw(img)
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Write some text
+draw.text((5, 5), "Hello World!", font=font, fill=(255, 255, 255))
+# Write buffer to display hardware, must be called to make things visible on the
+# display!
+disp.display(img)
+#time.sleep(60)
+#disp.reset()
