8d9bdaf93119980acff42d2d833530679de665b1
nathanhoyes <07nhoyes@gmail.com>
2023-03-02 20:57:34 +0000

    Added sensor test

---
+++ src/Sensors/SensorTest.py
@@ -0,0 +1,18 @@
+import adafruit_dht as DHT
+
+from board import D26
+from time import sleep
+
+sensor = DHT.DHT22(D26)
+while True:
+    try:
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        print("Temperature is: {.1f} C  Humidity is: {}%".format(temp, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+    except Exception as error:
+        sensor.exit()
+        raise error
+
+    sleep(2.0)
